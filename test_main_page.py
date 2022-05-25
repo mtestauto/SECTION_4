8768b581d1b112bc2fdc35b1bc40c2270d490705
mtestauto <m_test_auto@mail.ru>
2022-05-26 00:34:51 +0300

    В файл с тестами в первый тест добавил проверку из другой страницы с явной инициализации страницы в теле теста. Остальные проверки закомментировал

--- test_main_page.py
+++ test_main_page.py
@@ -7,30 +7,33 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_url()
 
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
-
-def test_login_url(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page1 = LoginPage(browser, link)
-    page1.open()
-    page1.should_be_login_url()
-
-def test_autorization_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page1 = LoginPage(browser, link)
-    page1.open()
-    page1.should_be_login_form()
-
-def test_registration_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page1 = LoginPage(browser, link)
-    page1.open()
-    page1.should_be_register_form()
+# def test_guest_should_see_login_link(browser):
+#     link = "http://selenium1py.pythonanywhere.com/"
+#     page = MainPage(browser, link)
+#     page.open()
+#     page.should_be_login_link()
+#
+# def test_login_url(browser):
+#     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+#     page1 = LoginPage(browser, link)
+#     page1.open()
+#     page1.should_be_login_url()
+#
+# def test_autorization_form(browser):
+#     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+#     page1 = LoginPage(browser, link)
+#     page1.open()
+#     page1.should_be_login_form()
+#
+# def test_registration_form(browser):
+#     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+#     page1 = LoginPage(browser, link)
+#     page1.open()
+#     page1.should_be_register_form()
+#
 
 
 
