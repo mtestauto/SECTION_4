af39bb33e76d2e16c902cb9ea163ebc548dcf10d
mtestauto <m_test_auto@mail.ru>
2022-05-26 00:21:52 +0300

    Добавлены методы для логин-пэйдж. Локаторы. Тесты.

---
+++ pages/login_page.py
@@ -0,0 +1,22 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес - OK
+        site_url = self.browser.current_url
+        assert "login" in site_url, f"String login in {site_url} is false"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.AUTHORIZATION_FORM), "NOK AUTH-FORM"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "NOK REG-FORM"
--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -12,5 +13,24 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_login_url(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page1 = LoginPage(browser, link)
+    page1.open()
+    page1.should_be_login_url()
+
+def test_autorization_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page1 = LoginPage(browser, link)
+    page1.open()
+    page1.should_be_login_form()
+
+def test_registration_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page1 = LoginPage(browser, link)
+    page1.open()
+    page1.should_be_register_form()
+
 
 
